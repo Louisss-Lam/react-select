251358ab98a801aa08f762dca1d70c08cb0c05a2
Louisss-Lam <louislam617@gmail.com>
2022-12-08 11:51:45 +0000

    feat: add multiselect logic

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,6 @@
 import { Select } from "./Select";
+import { useState } from "react";
+import { SelectOption } from "./Select";
 
 const options = [
   { label: "First", value: 1 },
@@ -9,9 +11,14 @@ const options = [
 ]
 
 function App() {
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
+
   return (
     <>
-      <Select options={options}/>
+      <Select multiple options={options} value={value1} onChange={ o => setValue1(o)}/>
+      <br />
+      <Select options={options} value={value2} onChange={ o => setValue2(o)}/>
     </>
   )  
 }
--- vite-project/src/Select.tsx
+++ vite-project/src/Select.tsx
@@ -1,26 +1,100 @@
 import styles from "./select.module.css";
+import { useEffect, useState } from "react";
 
-type SelectOption = {
+export type SelectOption = {
     label: string
-    value: any
+    value: string | number
+}
+
+type MultipleSelectProps = {
+    multiple: true
+    value: SelectOption[]
+    onChange: (value: SelectOption[]) => void
+}
+
+type SingleSelectProps = {
+    multiple?: false
+    value?: SelectOption
+    onChange: (value: SelectOption | undefined) => void
 }
 
 type SelectProps = {
     options: SelectOption[]
-    onChange: (value: SelectOption | undefined) => void
-    value?: SelectOption
-}
+} & (SingleSelectProps | MultipleSelectProps)
 
-export function Select({ value, onChange, options }: SelectProps) {
+export function Select({ multiple, value, onChange, options }: SelectProps) {
+    const [isOpen, setIsOpen] = useState(false);
+    const [highlightedIndex, setHighlightedIndex] = useState(0);
+
+
+    function clearOptions() {
+        multiple ? onChange([]) : onChange(undefined);
+    }
+
+    function selectOption(option: SelectOption) { 
+        if (multiple) {
+            if (value.includes(option)) {
+                onChange(value.filter(o => o !== option))
+            } else {
+                onChange([...value, option])
+            }
+        } else {
+            if (option !== value ) onChange(option);
+        }
+    }
+
+    function isOptionSelected(option: SelectOption) {
+        return multiple ? value.includes(option) : option === value;
+    }
+
+    useEffect(() => {
+        if(isOpen) setHighlightedIndex(0)
+    }, [isOpen])
+
     return (
-        <div tabIndex={0} className={styles.container}>
-            <span className={styles.value}>Value</span>
-            <button className={styles["clear-btn"]}>&times;</button>
+        <div 
+            onBlur={() => setIsOpen(false)}
+            onClick={() => setIsOpen(prev => !prev)}
+            tabIndex={0} 
+            className={styles.container}
+        >
+            <span className={styles.value}>{multiple ? value.map(v => (
+                <button key={v.value} onClick={e => {
+                    e.stopPropagation()
+                    selectOption(v)
+                }}
+                className={styles["option-badge"]}
+                >
+                    {v.label}
+                    <span className={styles["remove-btn"]}>&times;</span>
+                </button>
+            )) : value?.label}</span>
+            <button
+                className={styles["clear-btn"]}
+                onClick={e => {
+                    e.stopPropagation();
+                    clearOptions();
+                }}
+            >
+                &times;
+            </button>
             <div className={styles.divider}></div>
             <div className={styles.caret}></div>
-            <ul className={`${styles.options} ${styles.show}`}>
-                {options.map(option => (
-                    <li key={option.label} className={styles.option}>{option.label}</li>
+            <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
+                {options.map((option, index) => (
+                    <li 
+                        onClick={e => {
+                            e.stopPropagation();
+                            selectOption(option);
+                            setIsOpen(false);
+                        }}
+                        onMouseEnter={() => setHighlightedIndex(index)}
+                        key={option.value} 
+                        className={`${styles.option} ${isOptionSelected(option) ? styles.selected : ""}
+                        ${index === highlightedIndex ? styles.highlighted : ""}`}
+                    >
+                            {option.label}
+                    </li>
                 ))}
             </ul>
         </div>
